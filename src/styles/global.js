47339e363bb1cfead1af819bf7961c2a04d74b45
Rodrigo Barros <rodrigobarros110@hotmail.com>
2021-01-05 11:41:25 -0300

    Create global.js

---
+++ src/styles/global.js
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components';
+import MontserratRegular from '../fonts/Montserrat-Regular.ttf';
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+	@font-face {
+		font-family: Montserrat;
+		src: url(${MontserratRegular});
+    }
+    body {
+        -webkit-font-smoothing: antialiased !important;
+    }
+    body, input, button {
+		font: 14px 'Montserrat', sans-serif;
+    }
+    html, body, #root {
+        height: 100%;
+    }
+    a {
+        text-decoration: none;
+    }
+    ul {
+        list-style: none;
+    }
+    button {
+        cursor: pointer;
+    }
+	h1 {
+		color:red;
+		background-color: pink;
+	}
+`;
