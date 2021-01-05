d6ef7776fc59407047427f7a9cd2228b93e316bd
Rodrigo Barros <rodrigobarros110@hotmail.com>
2021-01-05 11:41:07 -0300

    Create breakpoints.js

---
+++ src/config/breakpoints.js
@@ -0,0 +1,13 @@
+const size = {
+	phone: '320px',
+	tablet: '768px',
+	desktop: '1024px',
+	monitor: '1280px',
+};
+
+export const device = {
+	phone: `(min-width: ${size.phone})`,
+	tablet: `(min-width: ${size.tablet})`,
+	desktop: `(min-width: ${size.desktop})`,
+	monitor: `(min-width: ${size.monitor})`,
+};
